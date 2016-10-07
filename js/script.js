981656c8b744eaf5eed6a2b393b9cda540b5f5fb
anamklone <amk226@pitt.edu>
2016-10-06 21:03:20 -0400

    Add error messages to form if input is invalid

--- js/script.js
+++ js/script.js
@@ -9,5 +9,23 @@
   });
   $('#thisForm').on('submit', function(e) {
     e.preventDefault();
+    if ($('input[name=firstName]').val() == null || $('input[name=firstName]').val() == "") {
+      $('label[for=firstName] + .alert').text('firstName field is not valid');
+    }
+    if ($('input[name=lastName]').val() == null || $('input[name=lastName]').val() == "") {
+      $('label[for=lastName] + .alert').text('lastName field is not valid');
+    }
+    if ($('input[name=email]').val() == null || $('input[name=email]').val() == "" || $('input[name=email]').val().indexOf("@") != -1) {
+      $('label[for=email] + .alert').text('email field is not valid');
+    }
+  });
+  $('input[name=firstName]').click(function() {
+    $('label[for=firstName] + .alert').text('');
+  });
+  $('input[name=lastName]').click(function() {
+    $('label[for=lastName] + .alert').text('');
+  });
+  $('input[name=email]').click(function() {
+    $('label[for=email] + .alert').text('');
   });
 })(jQuery);
